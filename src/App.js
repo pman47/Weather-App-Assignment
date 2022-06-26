728df939587250b3d00242ab783cd40958cdee92
Manish <manishprajapatim416@gmail.com>
2022-06-26 13:39:19 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,51 +1,28 @@
- import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
 import Axios from "axios";
 import CityComponent from "./components/CityComponent";
 import WeatherComponent from "./components/WeatherComponent";
-
-
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 380px;
-  padding: 20px 10px;
-  margin: auto;
-  border-radius: 4px;
-  box-shadow: 0 3px 6px 0 #555;
-  background: white;
-  font-family: Montserrat;
-`;
-
-const AppLabel = styled.span`
-  color: black;
-  margin: 20px auto;
-  font-size: 18px;
-  font-weight: bold;
-`;
-
+import "./App.css";
 
 function App() {
-  const [city, updateCity] = useState('');
+  const [city, updateCity] = useState("");
   const [weather, updateWeather] = useState();
   const fetchWeather = async (e) => {
     e.preventDefault();
     const response = await Axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`
     );
     updateWeather(response.data);
   };
   return (
-    <Container>
-      <AppLabel>React Weather App</AppLabel>
+    <div className="container">
+      <span className="title">React Weather App</span>
       {city && weather ? (
         <WeatherComponent weather={weather} city={city} />
       ) : (
         <CityComponent updateCity={updateCity} fetchWeather={fetchWeather} />
       )}
-    </Container>
+    </div>
   );
 }
 
