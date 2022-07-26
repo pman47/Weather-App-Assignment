edccea9eeda8a8108c087d723065d00ad0cc67b0
Manish <manishprajapatim416@gmail.com>
2022-07-26 15:30:52 +0530

    Done

--- src/components/WeatherInfoComponent.js
+++ src/components/WeatherInfoComponent.js
@@ -1,48 +1,24 @@
 import React from "react";
-import styled from "styled-components";
-
-
-const InfoLabel = styled.span`
-  display: flex;
-  flex-direction: column;
-  font-size: 14px;
-  margin: 15px;
-  & span {
-    font-size: 12px;
-    text-transform: capitalize;
-  }
-`;
-
-const InfoContainer = styled.div`
-  display: flex;
-  margin: 5px 10px;
-  flex-direction: row;
-  justify-content: space-evenly;
-  align-items: center;
-`;
-const InfoIcon = styled.img`
-  width: 36px;
-  height: 36px;
-`;
+import "../App.css";
 
 export const WeatherInfoIcons = {
-    sunset: "/icons/temp.svg",
-    sunrise: "/icons/temp.svg",
-    humidity: "/icons/humidity.svg",
-    wind: "/icons/wind.svg",
-    pressure: "/icons/pressure.svg",
+  sunset: "/icons/temp.svg",
+  sunrise: "/icons/temp.svg",
+  humidity: "/icons/humidity.svg",
+  wind: "/icons/wind.svg",
+  pressure: "/icons/pressure.svg",
 };
 
 const WeatherInfoComponent = (props) => {
-    const {name, value} = props;
-    return (
-        <InfoContainer>
-            <InfoIcon src={WeatherInfoIcons[name]}/>
-            <InfoLabel>
-                {value}
-                <span>{name}</span>
-            </InfoLabel>
-        </InfoContainer>
-    );
+  const { name, value } = props;
+  return (
+    <div className="InfoContainer">
+      <img className="InfoIcon" src={WeatherInfoIcons[name]} />
+      <span className="InfoLabel">
+        {value}
+        <span>{name}</span>
+      </span>
+    </div>
+  );
 };
 export default WeatherInfoComponent;
